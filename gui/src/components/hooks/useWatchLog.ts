c302b0d5964062b641add2109b81288ed883e846
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2026-01-12 08:11:39 +0900

    docs: add note to useWatchLog about root component usage for event listening

--- gui/src/components/hooks/useWatchLog.ts
+++ gui/src/components/hooks/useWatchLog.ts
@@ -3,6 +3,10 @@ import { useParameters } from "../providers/parameters/useParameters";
 import { useEffect } from "react";
 import { particleLogToString, type ParticleLogNormalized } from "../../api/log";
 
+/**
+ * NOTE: If we don't call this in the root component,
+    it will listen after the simulation runs and won't make it in time for the emit.
+ */
 export const useWatchLog = () => {
   const { dispatch } = useParameters();
 
